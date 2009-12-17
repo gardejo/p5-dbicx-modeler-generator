818f0d78c8b7bef761398d56d7528043a8fe6275
MORIYA Masaki (gardejo) <moriya@ermitejo.com>
2009-12-17 21:30:12 +0900

    Explicitly declared astrings of hyperlinks in PODs

--- examples/src/sbin/maintain_models.pl
+++ examples/src/sbin/maintain_models.pl
@@ -165,7 +165,7 @@ L<http://ttt.ermitejo.com>.
 
 This module is free software;
 you can redistribute it and/or modify it under the same terms as Perl itself.
-See L<perlgpl> and L<perlartistic>.
+See L<perlgpl|perlgpl> and L<perlartistic|perlartistic>.
 
 The full text of the license can be found in the F<LICENSE> file
 included with this distribution.
