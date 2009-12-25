d366587adf878df5b335ad2aa1e8d49a8f6b7bff
MORIYA Masaki (gardejo) <moriya@ermitejo.com>
2009-12-25 23:46:26 +0900

    Moved supplemental test to the general test suite

--- coverage.sh
+++ coverage.sh
@@ -1,14 +1,15 @@
 # for Unix, Linux, etc.
-export HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,inc,examples,perl/site/lib,perl/lib,t/,xt/
+export HARNESS_PERL_SWITCHES=-MDevel::Cover=+ignore,^inc/,^examples/,perl/site/lib/,perl/lib/,^t/,^xt/
 
 rm -rf cover_db
 # make realclean
 
-# perl Makefile.PL
-# make manifest
+perl Makefile.PL
+make manifest
+make
 
 # fixme: how we set 'also_private' option of Pod::Coverage here?
 
-prove -l xt/supplement.t t
+prove -l
 cover
 firefox cover_db/coverage.html &
